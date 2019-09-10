47f84a9d1a8c531d83ad444794e7dd915bf50c31
zedix <lx.vignal@gmail.com>
2019-09-10 12:10:13 +0200

    chore: configure storybook settings

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,5 @@
-import { configure, addDecorator } from '@storybook/polymer';
+import { configure, addDecorator, addParameters } from '@storybook/polymer';
+import { create } from '@storybook/theming';
 import { withA11y } from '@storybook/addon-a11y';
 import '@storybook/addon-console';
 
@@ -6,6 +7,16 @@ const req = require.context('../stories', true, /\.stories\.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+addParameters({
+  options: {
+    theme: create({
+      brandTitle: 'Zedix UI',
+      // brandImage: 'https://cdn.worldvectorlogo.com/logos/web-components.svg',
+    }),
+    //isToolshown: true,
+  },
+});
 
 addDecorator(withA11y);
 configure(loadStories, module);
