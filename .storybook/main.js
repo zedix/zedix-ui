1f740968c7b684b1af36c11f0b71347100a04f6e
zedix <lx.vignal@gmail.com>
2020-06-24 01:24:34 +0200

    fix: storybook build

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,7 @@ module.exports = {
     'storybook-prebuilt/addon-actions/register.js',
     'storybook-prebuilt/addon-docs/register.js',
     'storybook-prebuilt/addon-viewport/register.js',
+    'storybook-prebuilt/addon-a11y/register.js',
   ],
   esDevServer: {
     // custom es-dev-server options
