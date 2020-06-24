080b66ea3dc6b5cac87b25a45cb485b9be076475
zedix <lx.vignal@gmail.com>
2020-06-24 10:29:25 +0200

    refactor(storybook): cleanup storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,43 +1,19 @@
 // Configures the "preview" iframe that renders the components
 
-import {
-  addParameters,
-  addDecorator,
-  setCustomElements,
-  withA11y,
-} from '@open-wc/demoing-storybook';
+import { addParameters, setCustomElements } from '@open-wc/demoing-storybook';
 
-// import { create } from '@storybook/theming';
+addParameters({
+  docs: {
+    iframeHeight: '200px',
+  },
+});
 
 async function run() {
   const customElements = await (
     await fetch(new URL('../custom-elements.json', import.meta.url))
   ).json();
+
   setCustomElements(customElements);
-
-  addDecorator(withA11y);
-
-  addParameters({
-    a11y: {
-      config: {},
-      options: {
-        checks: { 'color-contrast': { options: { noScroll: true } } },
-        restoreScroll: true,
-      },
-    },
-    docs: {
-      iframeHeight: '200px',
-    },
-    options: {
-      // https://storybook.js.org/docs/configurations/theming/#create-a-theme-quickstart
-      //theme: create({
-      //base: 'light',
-      //brandTitle: 'Zedix UI — Web Components',
-      // brandImage: 'https://cdn.worldvectorlogo.com/logos/web-components.svg',
-      //}),
-      //isToolshown: true,
-    },
-  });
 }
 
 run();
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ export { ZxButton } from './components/zx-button/zx-button.js';
 export { ZxCheckbox } from './components/zx-checkbox/zx-checkbox.js';
 export { ZxInput } from './components/zx-input/zx-input.js';
 export { ZxInputStepper } from './components/zx-input-stepper/zx-input-stepper.js';
+export { ZxModal } from './components/zx-modal/zx-modal.js';
 export { ZxRadio } from './components/zx-radio/zx-radio.js';
 export { ZxRating } from './components/zx-rating/zx-rating.js';
 export { ZxSelect } from './components/zx-select/zx-select.js';
