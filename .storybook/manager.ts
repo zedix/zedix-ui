5bae1c7f2752819727155d6ba3de5c7cab101116
zedix <lx.vignal@gmail.com>
2023-10-21 14:03:05 +0200

    chore: yarn upgrade dependencies

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,13 +1,15 @@
-/*
 import { addons } from '@storybook/manager-api';
 import { create } from '@storybook/theming/create';
 
 addons.setConfig({
+  // https://storybook.js.org/docs/react/configure/theming#create-a-theme-quickstart
   theme: create({
     base: 'dark',
-    brandTitle: 'Zedix UI — Web Components',
+    brandTitle: `
+      <div style="display: flex; gap: 12px; align-items: center; font-size: 24px">
+        <img src="https://raw.githubusercontent.com/webcomponents/webcomponents-icons/master/logo/logo_512x512.png" width="40" />
+        Zedix UI — Web Components
+      </div>`,
     brandUrl: 'https://github.com/zedix/zedix-ui',
-    brandImage: 'https://cdn.worldvectorlogo.com/logos/web-components.svg',
   }),
 });
-*/
